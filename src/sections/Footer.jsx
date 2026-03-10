a29970e55e9666dd455c20b967d1b36f0fa94a2f
Mustafa Khamis <safsafrwanda2006@gmail.com>
2026-03-10 04:48:36 +0200

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,18 +4,16 @@ import "./footer.css";
 function Footer() {
   return (
     <footer className="platform-footer">
-
       <div className="footer-container">
-
         {/* about */}
         <div className="footer-col">
           <a href="/" className="footer-logo">
-          <h3>SudanTeach</h3>
-          <img src="/icons/logo.png" alt="" />
+            <h3>SudanTeach</h3>
+            <img src="/icons/logo.png" alt="" />
           </a>
           <p>
-            منصة تعليمية تهدف إلى ربط الطلاب بالمدرسين والدورات التعليمية
-            في مكان واحد، لتسهيل الوصول إلى المعرفة والتعلم بطريقة حديثة.
+            منصة تعليمية تهدف إلى ربط الطلاب بالمدرسين والدورات التعليمية في
+            مكان واحد، لتسهيل الوصول إلى المعرفة والتعلم بطريقة حديثة.
           </p>
         </div>
 
@@ -23,10 +21,18 @@ function Footer() {
         <div className="footer-col">
           <h4>روابط سريعة</h4>
           <ul>
-            <li><a href="#about">عن المنصة</a></li>
-            <li><a href="#courses">الدورات</a></li>
-            <li><a href="#submit-course">إضافة دورة</a></li>
-            <li><a href="#contact">تواصل معنا</a></li>
+            <li>
+              <a href="#about">عن المنصة</a>
+            </li>
+            <li>
+              <a href="#courses">الدورات</a>
+            </li>
+            <li>
+              <a href="#submit-course">إضافة دورة</a>
+            </li>
+            <li>
+              <a href="#contact">تواصل معنا</a>
+            </li>
           </ul>
         </div>
 
@@ -43,68 +49,75 @@ function Footer() {
         {/* contact */}
         <div className="footer-col">
           <h4>تواصل معنا</h4>
-
-          <p>📞 +250794101251</p>
-          <p>✉️ sudanteach.platform@gmail.com</p>
-
-          <div className="footer-social">
-
-            <a href="https://x.com/SudanTeach46689"
-            className="social-icon"
+          <a
+            className="direct-logo"
+            href="https://wa.me/250722727821"
             target="_blank"
+          >
+            <div className="social-icon">
+              <img src="/icons/whatsapp.png" alt="whatsapp" />
+            </div>
+            <p> 0722727821</p>
+          </a>
+          <a
+            className="direct-logo"
+            href="mailto:sudanteach@gmail.com"
+            aria-label="Email"
+          >
+            <div className="social-icon">
+              <img src="/icons/email.png" alt="Email" />
+            </div>
+
+            <p> sudanteach.platform@gmail.com</p>
+          </a>
+          <div className="footer-social">
+            <a
+              href="https://x.com/sudanteach"
+              className="social-icon"
+              target="_blank"
             >
               <img src="/icons/x.png" alt="" />
             </a>
 
-            <a href="https://www.facebook.com/profile.php?id=61585006871278" className="social-icon"
-            target="_blank">
+            <a
+              href="https://www.facebook.com/profile.php?id=61585006871278"
+              className="social-icon"
+              target="_blank"
+            >
               <img src="/icons/facebook.png" alt="facebook" />
             </a>
 
-            <a href="https://www.instagram.com/sudanteach" 
-            className="social-icon"
-            target="_blank"
+            <a
+              href="https://www.instagram.com/sudanteach"
+              className="social-icon"
+              target="_blank"
             >
               <img src="/icons/instgram.png" alt="instagram" />
             </a>
 
-            <a 
-            href="https://www.youtube.com/@SudanTeach" 
-            className="social-icon"
-            target="_blank"
+            <a
+              href="https://www.youtube.com/@SudanTeach"
+              className="social-icon"
+              target="_blank"
             >
-
               <img src="/icons/youtube.png" alt="youtube" />
             </a>
-
           </div>
-
         </div>
-
       </div>
 
       <div className="footer-bottom">
-
-        <p>
-          © {new Date().getFullYear()} SudanTeach - جميع الحقوق محفوظة
-        </p>
+        <p>© {new Date().getFullYear()} SudanTeach - جميع الحقوق محفوظة</p>
 
         <p className="footer-dev">
-          تم التطوير بواسطة <a 
-          href="https://execode-two.vercel.app/"
-          target="_blank"
-          ><strong>ExeCode</strong></a>
-          
+          تم التطوير بواسطة{" "}
+          <a href="https://execode-two.vercel.app/" target="_blank">
+            <strong>ExeCode</strong>
+          </a>
         </p>
-
       </div>
-
     </footer>
   );
 }
 
 export default Footer;
-
-
-
-
